9473e19081321f199b9703e2817a995b0115e189
ilianarojasswy15-design <ilianarojasswy15@gmail.com>
2025-11-17 11:59:44 +0000

    ejercicios_hasta_17nov

--- .learn/resets/12.4-Map_list_of_objects/app.py
+++ .learn/resets/12.4-Map_list_of_objects/app.py
@@ -15,10 +15,8 @@ def calculate_age(date_of_birth):
 
 def format_greeting(person):
     # Your code here
-    name = person["name"]
-    age = calculate_age(person["birth_date"])
-    return f"Hello, my name is {name} and I am {age} years old"
-    #return person["name"]
+    return person["name"]
+
 
 name_list = list(map(format_greeting, people))
 
--- exercises/12.5-Yes_and_no/app.py
+++ exercises/12.5-Yes_and_no/app.py
@@ -1,4 +1,13 @@
 the_bools = [0,1,0,0,1,1,1,0,0,1,0,1,1,0,0,0,0,0,0,0,0,1,0,0,0,0,1]
 
 # Your code here
+def wiki_swap(bool):
+    ki = "wiki"
+    ko = "woko"
+    if bool == 0:
+        return ko
+    else:
+        return ki
 
+new_list = list(map(wiki_swap,the_bools))
+print(new_list)
