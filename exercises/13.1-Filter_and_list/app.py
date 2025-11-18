97fdf59f10e3f4aed76a5d21b1643db0292b0227
ilianarojasswy15-design <ilianarojasswy15@gmail.com>
2025-11-18 12:13:07 +0000

    ejercicios hasta 18nov

--- exercises/13.1-Filter_and_list/app.py
+++ exercises/13.1-Filter_and_list/app.py
@@ -1,6 +1,8 @@
 all_names = ["Romario", "Boby", "Roosevelt", "Emiliy", "Michael", "Greta", "Patricia", "Danzalee"]
 
 # Your code here
+find_char = "R"
+resulting_names = list(filter(lambda name: find_char in name, all_names))
 
 print(resulting_names)
 
